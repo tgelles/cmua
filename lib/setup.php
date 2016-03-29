9e2990a9126af59430b84ae7ffcda75d2412a8e5
tgelles <tgelles@jhu.edu>
2016-03-29 15:07:56 -0400

    backstretch.js no longer creating jshint errors
    backstretch.js is 3rd party script. should not be correcting errors
    discovered by jshint

--- lib/setup.php
+++ lib/setup.php
@@ -106,7 +106,7 @@ function assets() {
   }
 
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
-  wp_enqueue_script('sage/backstretch_js', Assets\asset_path('scripts/backstretch.js'), ['sage/js'], null, true);
+  wp_enqueue_script('backstretch_js', asset_path('scripts/backstretch.js'), [], null, true);
 
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
